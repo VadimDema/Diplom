9ad4c1e856fe6bf4daffce4d315fe75121628653
VadimDema <90608445+VadimDema@users.noreply.github.com>
2023-04-16 09:03:31 +0300

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,229 @@
+let excercise = [
+  {
+    title: "Подъёмы гантелей сидя",
+    category: "arms",
+    img: "/Assets/Img/biceps.jpg",
+    description:
+      "Сядьте на наклонную скамью с гантелями в руках, прижмите тело к спинке, стопы — к полу. Опустите руки, чтобы они свободно свисали вниз, а локти находились за линией тела. Согните руки с гантелями, затем плавно и под контролем верните в исходное положение и повторите.",
+    link: "https://youtu.be/5m7QrfXEavM",
+  },
+  {
+    title: "Подъём штанги на бицепс",
+    category: "arms",
+    img: "/Assets/Img/biceps_2.jpg",
+    description:
+      "Возьмите штангу обратным хватом, согните локти и поднимите её до уровня плеч. Опустите обратно и повторите. Следите, чтобы двигались только руки, а остальное тело оставалось статичным: никакой раскачки быть не должно.",
+    link: "https://www.youtube.com/watch?v=FqF11YdwxrQ",
+  },
+  {
+    title: "Отжимания на брусьях",
+    category: "arms",
+    img: "/Assets/Img/triceps.jpg",
+    description:
+      "Запрыгните на брусья, опустите плечи и сведите лопатки, вытяните тело в одну линию. Согните локти и опуститесь вниз до параллели плеч с полом, но не ниже, чтобы не травмировать плечевой сустав. Выжмите себя обратно и повторите.",
+    link: "https://www.youtube.com/watch?v=jlX_8EtaeCs",
+  },
+  {
+    title: "Жим штанги стоя",
+    category: "shoulders",
+    img: "/Assets/Img/shoulders.jpg",
+    description:
+      "Это упражнение хорошо нагружает и плечи, и трицепс. Возьмите штангу на грудь, выведите локти вперёд. Выжмите штангу вверх и уведите за голову. Опустите в исходное положение и повторите.",
+    link: "https://youtu.be/0oj5pY3RQrc",
+  },
+  {
+    title: "Разводка гантелей в стороны",
+    category: "shoulders",
+    img: "/Assets/Img/shoulders_2.jpg",
+    description:
+      "Встаньте прямо, возьмите гантели, разверните руки ладонями к себе — это исходное положение. Чуть согните локти, разведите руки в стороны до параллели с полом. Опустите в исходное положение и повторите.",
+    link: "https://www.youtube.com/watch?v=mRScm8cs_EM",
+  },
+  {
+    title: "Жим гантелей лёжа",
+    category: "chest",
+    img: "/Assets/Img/bench.jpg",
+    description:
+      "Медленно поднимите гантели вертикально вверх. Руки при этом вытянуты, но не вывернуты в локтях, запястья прямые. Аккуратно опустите руки, согнув их в локтях, гантели держите горизонтально.",
+    link: "https://youtu.be/9CxcgGl5E7E",
+  },
+  {
+    title: "Разводка гантелей лёжа на наклонной скамье",
+    category: "chest",
+    img: "/Assets/Img/bench_2.jpg",
+    description:
+      "Плавно опускайте руки в стороны до тех пор, пока предплечья не будут параллельны полу. Локти при этом должны быть слегка согнуты. Чтобы достичь максимального растяжения грудных мышц, подайте грудную клетку чуть вверх, в противоход рукам. После чего вернитесь в исходное положение.",
+    link: "https://youtu.be/xwtaHancCQc",
+  },
+  {
+    title: "Отжимания от пола средним хватом",
+    category: "chest",
+    img: "/Assets/Img/bench_3.jpg",
+    description:
+      "На вдохе плавно опуститесь к полу, сгибая руки в локтях. Продолжайте до тех пор, пока не коснётесь грудью пола. На выдохе вернитесь в исходное положение, полностью выпрямив руки.",
+    link: "https://youtu.be/c902b5g8w0A",
+  },
+  {
+    title: "Подтягивания прямым хватом",
+    category: "back",
+    img: "/Assets/Img/back.jpg",
+    description:
+      "Возьмитесь за турник, опустите плечи и сведите лопатки. Подтянитесь, чтобы подбородок вышел за уровень турника, опуститесь обратно и повторите. Во время подтягивания не применяйте рывки и раскачку.",
+    link: "https://youtu.be/VUkBrVhRlv0",
+  },
+  {
+    title: "Тяга штанги в наклоне",
+    category: "back",
+    img: "/Assets/Img/back_2.jpg",
+    description:
+      "Поднимите штангу с пола и выпрямитесь в тазобедренных и коленных суставах. Отведите таз назад, слегка согните колени и наклонитесь вперёд с прямой спиной, удерживая гриф в вытянутых руках.",
+    link: "https://www.youtube.com/watch?v=njGz6T135lQ",
+  },
+  {
+    title: "Приседания со штангой",
+    category: "legs",
+    img: "/Assets/Img/squats.jpg",
+    description:
+      "Возьмите штангу со стоек, сведите лопатки, расправьте грудь, выпрямите спину. Поставьте ноги на ширине плеч, чуть разверните носки в стороны. Отведите таз назад и присядьте до параллели бёдер с полом.",
+    link: "https://www.youtube.com/watch?v=GotRb2Ob7MA",
+  },
+  {
+    title: "Выпады со штангой",
+    category: "legs",
+    img: "/Assets/Img/legs_2.jpg",
+    description:
+      "Встаньте прямо, возьмите в руки утяжеляющие элементы, расправьте плечи, выпрямите спину. Сделайте выпад вперёд, не касайтесь пола коленом стоящей сзади ноги. Выпрямитесь и повторите с другой ноги.",
+    link: "https://www.youtube.com/watch?v=HF0REfhKpfw",
+  },
+  {
+    title: "Становая тяга",
+    category: "legs",
+    img: "/Assets/Img/deadlift.jpg",
+    description:
+      "Не раскачиваясь и не перенося вес тела на носки, сорвите штангу движением мышц ног – квадрицепсов и ягодиц. После того как штанга прошла 20-30% амплитуды, выпрямите поясницу, выталкивая таз вперед, и зафиксируйте позицию.",
+    link: "https://www.youtube.com/watch?v=108Ga1EdL_Q",
+  },
+  {
+    title: "Скручивания",
+    category: "abs",
+    img: "/Assets/Img/press.jpg",
+    description:
+      "Выпрямите руки над головой или заведите их за голову. Сделайте выдох, напрягите пресс и задержите дыхание. Оторвите голову, плечи и лопатки от пола, поясницу оставьте прижатой к нему. Медленно вернитесь в исходное положение и повторите.",
+    link: "https://www.youtube.com/watch?v=TnzEMaU3v78",
+  },
+  {
+    title: "Велосипед",
+    category: "abs",
+    img: "/Assets/Img/byclie.jpg",
+    description:
+      "Уберите руки за голову и разведите локти в стороны. Не надо складывать руки в замок или давить ладонями на затылок — пальцы лишь слегка касаются головы. Поочерёдно сгибайте и выпрямляйте ноги, как будто крутите педали велосипеда.",
+    link: "https://www.youtube.com/watch?v=RfbFnd1P36g",
+  },
+];
+
+
+var burgerMenu = document.getElementById("burger-menu");
+var overlay = document.getElementById("menu");
+burgerMenu.addEventListener("click", function () {
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
+
+window.addEventListener('load', () => {
+	const form = document.querySelector("#new-task-form");
+	const input = document.querySelector("#new-task-input");
+	const list_el = document.querySelector("#tasks");
+
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
+
+		const task = input.value;
+
+		const task_el = document.createElement('div');
+		task_el.classList.add('task');
+
+		const task_content_el = document.createElement('div');
+		task_content_el.classList.add('content');
+
+		task_el.appendChild(task_content_el);
+
+		const task_input_el = document.createElement('input');
+		task_input_el.classList.add('text');
+		task_input_el.type = 'text';
+		task_input_el.value = task;
+		task_input_el.setAttribute('readonly', 'readonly');
+
+		task_content_el.appendChild(task_input_el);
+
+		const task_actions_el = document.createElement('div');
+		task_actions_el.classList.add('actions');
+
+		const task_edit_el = document.createElement('button');
+		task_edit_el.classList.add('edit');
+		task_edit_el.innerText = 'Изменить';
+
+		const task_delete_el = document.createElement('button');
+		task_delete_el.classList.add('delete');
+		task_delete_el.innerText = 'Удалить';
+
+		task_actions_el.appendChild(task_edit_el);
+		task_actions_el.appendChild(task_delete_el);
+
+		task_el.appendChild(task_actions_el);
+
+		list_el.appendChild(task_el);
+
+		input.value = '';
+
+		task_edit_el.addEventListener('click', (e) => {
+			if (task_edit_el.innerText.toLowerCase() == "edit") {
+				task_edit_el.innerText = "Save";
+				task_input_el.removeAttribute("readonly");
+				task_input_el.focus();
+			} else {
+				task_edit_el.innerText = "Edit";
+				task_input_el.setAttribute("readonly", "readonly");
+			}
+		});
+
+		task_delete_el.addEventListener('click', (e) => {
+			list_el.removeChild(task_el);
+		});
+	});
+});
+
+
+let out = "";
+excercise.forEach((excercise) => {
+  out += `
+  <div class = "excercise-item">
+          <div class = "excercise-img">
+              <img src = "${excercise.img}" alt = "excercise">
+          </div>
+          <div class = "excercise-name">
+              <h3>${excercise.title}</h3>
+              <p class = "excercise-description">"${excercise.description}"</p>
+              <a href = "${excercise.link}" class = "excercise-btn">Посмотреть видео</a>
+          </div>
+   </div>
+
+  `;
+});
+
+document.getElementById("out").innerHTML = out;
+
+
+function liveSearch() {
+  let cards = document.querySelectorAll(".excercise-item");
+  let search_query = document.getElementById("searchbox").value;
+  for (var i = 0; i < cards.length; i++) {
+    if (cards[i].innerText.toLowerCase().includes(search_query.toLowerCase())) {
+      cards[i].classList.remove("is-hidden");
+    } else {
+      cards[i].classList.add("is-hidden");
+    }
+  }
+}
+
+
+
